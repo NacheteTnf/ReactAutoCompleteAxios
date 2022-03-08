43626e8a1b8ad0bb1324d761bc559f54cdfd7860
NacheteTnf <ignaciocabrerahdez97@gmail.com>
2022-03-08 16:53:17 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './custom.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 function App() {
+  const [users, setUsers] = useState([]);
+  const [text, setText] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
+  useEffect(() => {
+    const loadUsers = async()=>{
+      const response = await axios.get('https://reqres.in/api/users');
+      setUsers(response.data.data)
+    }
+    loadUsers();
+  }, [])
+
+  const onSuggestHandler = (text) =>{
+    setText(text);
+    setSuggestions([]);
+  }
+  const onChangeHandler = (text) =>{
+    let matches = []
+    if(text.length>0){
+      matches = users.filter(user=>{
+        const regex = new RegExp(`${text}` , "gi");
+        return user.email.match(regex)
+      })
+    }
+    setSuggestions(matches)
+    setText(text)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      
+      <input type="text"
+      className='col-md-12 input'
+      style={{marginTop:10}}
+      onChange={e=>onChangeHandler(e.target.value)}
+      value={text}
+      onBlur={()=>{
+        setTimeout(()=>{
+          setSuggestions([])
+        }, 100);
+      }}
+      />
+      {suggestions && suggestions.map((suggestions, i)=>
+        <div key={i} className="suggestion col-md-12 justify-content-md-center"
+        onClick={()=> onSuggestHandler(suggestions.email)}
+        >{suggestions.email}</div>
+      )}
     </div>
   );
 }
